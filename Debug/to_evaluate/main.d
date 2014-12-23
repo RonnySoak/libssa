4537092b2485ffbbc8a122737013f33a8579f238
Ronny Soak <ronny.soak@gmail.com>
2014-12-23 15:27:54 +0100

    removed Debug folder

--- Debug/to_evaluate/main.d
+++
@@ -1,3 +0,0 @@
-to_evaluate/main.d: ../to_evaluate/main.c ../to_evaluate/swalign.h
-
-../to_evaluate/swalign.h:
--- Debug/to_evaluate/swalign.d
+++
@@ -1,3 +0,0 @@
-to_evaluate/swalign.d: ../to_evaluate/swalign.c ../to_evaluate/swalign.h
-
-../to_evaluate/swalign.h:
